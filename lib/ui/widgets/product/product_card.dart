61b7359e55533464502714a97378ba3d8ffb9795
RUS0 <alejandrofan2@gmail.com>
2022-06-02 18:55:27 +0200

    bug: not open product page if not clik on image, now yes

--- lib/ui/widgets/product/product_card.dart
+++ lib/ui/widgets/product/product_card.dart
@@ -25,73 +25,75 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        clipBehavior: Clip.none,
-        width: 170,
-        padding: EdgeInsets.all(10),
-        margin: EdgeInsets.only(right: 12, top: 1, bottom: 1, left: 1),
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => ProductPage(
-                              id: id,
-                              image: image,
-                              title: name,
-                              brand: brand,
-                              description: description,
-                              price: price,
-                              tags: tags,
-                            )));
-              },
-              child: Container(
-                child: Image.asset(
-                  image,
-                  fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (BuildContext context) => ProductPage(
+                    id: id,
+                    image: image,
+                    title: name,
+                    brand: brand,
+                    description: description,
+                    price: price,
+                    tags: tags,
+                  ))),
+      child: Card(
+        child: Container(
+          clipBehavior: Clip.none,
+          width: 170,
+          padding: EdgeInsets.all(10),
+          margin: EdgeInsets.only(right: 12, top: 1, bottom: 1, left: 1),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: 150,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Image.asset(
+                    image,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              name,
-              style: TextStyle(
-                  fontSize: 16,
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                name,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Provider.of<ThemeProvider>(context).isDarkMode
+                        ? DarkTheme.reverseColor
+                        : LightTheme.reverseColor,
+                    fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                brand,
+                style: TextStyle(
+                  fontSize: 14,
                   color: Provider.of<ThemeProvider>(context).isDarkMode
                       ? DarkTheme.reverseColor
                       : LightTheme.reverseColor,
-                  fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              brand,
-              style: TextStyle(
-                fontSize: 14,
-                color: Provider.of<ThemeProvider>(context).isDarkMode
-                    ? DarkTheme.reverseColor
-                    : LightTheme.reverseColor,
+                ),
               ),
-            ),
-            Text(
-              "\$ $price",
-              style: TextStyle(
-                  fontSize: 14,
-                  color: Provider.of<ThemeProvider>(context).isDarkMode
-                      ? DarkTheme.primaryColor
-                      : LightTheme.primaryColor,
-                  fontWeight: FontWeight.w700),
-            ),
-          ],
+              Text(
+                "$price€",
+                style: TextStyle(
+                    fontSize: 14,
+                    color: Provider.of<ThemeProvider>(context).isDarkMode
+                        ? DarkTheme.primaryColor
+                        : LightTheme.primaryColor,
+                    fontWeight: FontWeight.w700),
+              ),
+            ],
+          ),
         ),
       ),
     );
